12038b005597193c292229cbd687e7a1877488b7
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-01-16 00:05:02 +0530

    day2 problems

---
+++ PROBLEM B GRIND/B. Maximum Sum.cpp
@@ -0,0 +1,121 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+bool SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    if (prime[n])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    ain(i, a, n);
+    sort(a.begin(), a.end());
+    vector<int> sum(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+    {
+        sum[i] = sum[i - 1] + a[i - 1];
+    }
+    int ans = 0;
+    for (int i = 0; i <= k; i++)
+    {
+        ans = max(ans, sum[n - (k - i)] - sum[2 * i]);
+    }
+    cout(ans);
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    // solve();
+}
